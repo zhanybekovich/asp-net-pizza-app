23256500c0366f3e37237bfa9435566884f250a3
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-05-16 16:31:13 +0600

    Add: post, put, delete actions

--- CondozoPizza/Controllers/PizzaController.cs
+++ CondozoPizza/Controllers/PizzaController.cs
@@ -25,5 +25,39 @@ namespace CondozoPizza.Controllers
 
             return pizza;
         }
+
+        [HttpPost]
+        public IActionResult Create(Pizza pizza)
+        {
+            PizzaService.Add(pizza);
+            return CreatedAtAction(nameof(Get), new { id = pizza.Id }, pizza);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Pizza pizza)
+        {
+            if (id != pizza.Id)
+                return BadRequest();
+
+            var existingPizza = PizzaService.Get(id);
+            if (existingPizza == null)
+                return NotFound();
+
+            PizzaService.Update(pizza);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var pizza = PizzaService.Get(id);
+            if (pizza is null)
+                return NotFound();
+
+            PizzaService.Delete(id);
+
+            return NoContent();
+        }
     }
 }
