1584de612d51709a98f50c76e9e82002ba1189f0
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-05-16 16:15:32 +0600

    Add project files.

---
+++ CondozoPizza/Controllers/PizzaController.cs
@@ -0,0 +1,29 @@
+﻿using CondozoPizza.Models;
+using CondozoPizza.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CondozoPizza.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzaController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Pizza>> GetAll() =>
+            PizzaService.GetAll();
+
+        [HttpGet("{id}")]
+        public ActionResult<Pizza> Get(int id)
+        {
+            var pizza = PizzaService.Get(id);
+
+            if (pizza == null)
+            {
+                return NotFound();
+            }
+
+            return pizza;
+        }
+    }
+}
